f2b07e6024d25826da7aae91243240e42a04b671
Drock4440 <daltonrothrock8@gmail.com>
2021-10-06 14:42:05 -0600

    fixing bug in routes

--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,60 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 const Workout = require("../models/workouts.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.put('/api/workouts/:id', async (req, res) => {
+  try {
+    const newExercise = await Workout.findByIdAndUpdate(req.params.id,
+      {
+        $push: {
+          exercises: req.body
+        }
+      },
+      { new: true })
+    res.json(newExercise);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-    console.log(body)
-    Workout.findByIdAndUpdate(params.id,
-        { $push: { exercises: body } }, { new: true })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.post("/api/workouts", async (req, res) => {
+  try {
+    const newWorkout = await Workout.create(req.body);
+    res.json(newWorkout);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.get('/api/workouts', async (req, res) => {
+  try {
+    const totalDuration = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" }
+        }
+      },
+    ]);
+    res.json(totalDuration);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-        .limit(10)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.get('/api/workouts/range', async (req, res) => {
+  try {
+    const totalDuration = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" }
+        }
+      }
+    ]).sort({ _id: 1 }).limit(7)
+    res.json(totalDuration);
+    console.log(totalDuration);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-
-router.delete("/api/workouts", ({ body }, res) => {
-    Workout.findByIdAndRemove(body.id)
-        .then(() => {
-            res.json(true);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
-
 
 module.exports = router;
